639ed84d284ded88642666422c2c4a54a905dc28
SiwarHaddad <96549904+SiwarHaddad@users.noreply.github.com>
2022-05-18 13:06:26 +0200

    Update dblouervoiture.sql

--- dblouervoiture.sql
+++ dblouervoiture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 09 mai 2022 à 04:12
+-- Généré le : mer. 18 mai 2022 à 07:26
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -39,15 +39,16 @@ CREATE TABLE IF NOT EXISTS `client` (
   `telClient` varchar(8) NOT NULL,
   `adresseClient` varchar(90) NOT NULL,
   PRIMARY KEY (`idClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
 --
 
 INSERT INTO `client` (`idClient`, `nomClient`, `prenomClient`, `mdpClient`, `pseudo`, `telClient`, `adresseClient`) VALUES
-(1, 'nom1', 'prenom1', 'mdp1', 'pseudo1', '00000000', 'adresse1'),
-(3, 'aa', 'aa', 'aa', 'aa', 'aa', 'aa');
+(5, 'admin', 'admin', 'mdpAdmin', 'admin', '00000000', 'adresse Admin\r\n'),
+(13, 'nom1', 'prenom1', 'mdp1', 'pseudo1', '11111111', 'adresse1'),
+(14, 'nom2', 'prenom2', 'mdp2', 'pseudo2', '22222222', 'adresse2');
 
 -- --------------------------------------------------------
 
@@ -57,24 +58,27 @@ INSERT INTO `client` (`idClient`, `nomClient`, `prenomClient`, `mdpClient`, `pse
 
 DROP TABLE IF EXISTS `location`;
 CREATE TABLE IF NOT EXISTS `location` (
-  `numL` int(11) NOT NULL AUTO_INCREMENT,
-  `matriculeV` varchar(30) NOT NULL,
-  `idClient` varchar(30) NOT NULL,
+  `idLocation` int(11) NOT NULL AUTO_INCREMENT,
+  `idVoiture` int(11) NOT NULL,
+  `idClient` int(6) NOT NULL,
   `date_locat` date DEFAULT NULL,
   `duree_prev` smallint(6) NOT NULL,
-  PRIMARY KEY (`numL`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+  `total_locat` decimal(12,2) NOT NULL,
+  PRIMARY KEY (`idLocation`),
+  KEY `fk_LocationVoiture` (`idVoiture`),
+  KEY `fk_LocationClient` (`idClient`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `location`
 --
 
-INSERT INTO `location` (`numL`, `matriculeV`, `idClient`, `date_locat`, `duree_prev`) VALUES
-(3, 'TX441', '66010177', '2020-10-26', 4),
-(4, 'MT005', '62991213', '2020-10-24', 2),
-(5, 'BS346', '66559090', '2020-10-26', 3),
-(7, 'BU671', '66050522', '2020-10-26', 1),
-(11, 'MT007', '66171709', '2020-10-26', 1);
+INSERT INTO `location` (`idLocation`, `idVoiture`, `idClient`, `date_locat`, `duree_prev`, `total_locat`) VALUES
+(25, 12, 14, '2022-05-10', 4, '55.22'),
+(26, 7, 13, '2022-05-17', 3, '120.00'),
+(28, 2, 13, '2022-05-16', 1, '555.00'),
+(29, 12, 14, '2022-05-19', 2, '70000.00'),
+(30, 7, 14, '2022-05-20', 2, '190000.00');
 
 -- --------------------------------------------------------
 
@@ -87,10 +91,10 @@ CREATE TABLE IF NOT EXISTS `voiture` (
   `idVoiture` int(11) NOT NULL AUTO_INCREMENT,
   `matricule` varchar(30) NOT NULL,
   `nom` varchar(60) NOT NULL,
-  `prix_locat` decimal(9,2) DEFAULT NULL,
+  `prix_locat` decimal(12,2) DEFAULT NULL,
   `disponible` varchar(10) NOT NULL,
   PRIMARY KEY (`idVoiture`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `voiture`
@@ -98,22 +102,31 @@ CREATE TABLE IF NOT EXISTS `voiture` (
 
 INSERT INTO `voiture` (`idVoiture`, `matricule`, `nom`, `prix_locat`, `disponible`) VALUES
 (1, 'BS345', 'BUS', '95000.00', 'oui'),
-(2, 'BS346', 'BUS', '95000.00', 'non'),
-(3, 'BU671', 'BUGGATI L9', '115000.00', 'non'),
+(2, 'BS346', 'BUS', '95000.00', 'oui'),
+(3, 'BU671', 'BUGGATI L9', '115000.00', 'oui'),
 (4, 'FR890', 'FERRARI ENZO', '155000.00', 'oui'),
-(5, 'HI678', 'HILUX 2C', '45000.00', 'oui'),
+(5, 'HI678', 'HILUX 2C', '45000.00', 'non'),
 (6, 'HU323', 'HUMMER A5', '90000.00', 'oui'),
-(7, 'LM234', 'LIMOUSINE B4', '95000.00', 'oui'),
-(8, 'MT005', 'MINIBUSss', '50000.00', 'non'),
+(7, 'LM234', 'LIMOUSINE B4', '95000.00', 'non'),
+(8, 'MT005', 'MINIBUSss', '50000.00', 'oui'),
 (9, 'MT006', 'MINIBUS', '50000.00', 'oui'),
 (10, 'MT00777', 'MINIBUS', '50000.00', 'non'),
-(11, 'TX441', 'TAXI', '35000.00', 'non'),
+(11, 'TX441', 'TAXI', '35000.00', 'oui'),
 (12, 'TX442', 'TAXI', '35000.00', 'oui'),
 (13, 'TX444', 'TAXI', '35000.00', 'oui'),
-(25, 'kk', 'kk', '22.00', 'mm'),
-(26, 'aa', 'aa', '11.11', 'non'),
-(27, 'aa', 'aa', '100.00', 'oui'),
-(28, 'aabb', 'aaaa', '120.00', 'non');
+(32, 'PP765', 'MAX5m', '90.00', 'oui'),
+(33, 'JJ50M', 'NJ5lo', '450.00', 'oui');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `fk_LocationClient` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_LocationVoiture` FOREIGN KEY (`idVoiture`) REFERENCES `voiture` (`idVoiture`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
