98ed683089afe818e0ade750029eb9294c226e64
SiwarHaddad <96549904+SiwarHaddad@users.noreply.github.com>
2022-05-22 22:56:23 +0200

    Update E_modifClient.java

--- src/presentation/events/events_client/E_modifClient.java
+++ src/presentation/events/events_client/E_modifClient.java
@@ -9,27 +9,39 @@ import dao.Client_dao;
 import dao.Voiture_dao;
 import metier.Client;
 import metier.Voiture;
+import presentation.F_accueil;
 import presentation.F_formVoiture;
 import presentation.F_gestionVoiture;
+import presentation.F_modifCompte;
 import presentation.exceptions.voiture.NoRowSelectedException;
 
 public class E_modifClient implements ActionListener {
 
-	private Client client;
+	private Client client = new Client();
 
-	public E_modifClient(Client client) {
-		this.client = client;
+	public E_modifClient() {
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		//System.out.println(client.getAdresseClient());
+		F_modifCompte Modif = new F_modifCompte();
+		
+		client.setIdClient(F_accueil.idConnected);
+		client.setNomClient(Modif.getContenuNom().getText());
+		client.setPrenomClient(Modif.getContenuPrenom().getText());
+		client.setAdresseClient(Modif.getContenuAdresse().getText());
+		client.setTelClient(Modif.getContenuTel().getText());
+		client.setPseudo(Modif.getContenuPseudo().getText());
+		
 		Client_dao D_client = new Client_dao();
 		
 		int r = D_client.update(client);
+		
 		if(r==1)
-			JOptionPane.showMessageDialog(null,"Modification avec succés","Message",JOptionPane.INFORMATION_MESSAGE);
-		else
-			JOptionPane.showMessageDialog(null,"Modifi","Message",JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null,"Modification avec succÃ©s","Message",JOptionPane.INFORMATION_MESSAGE);
 		
 	}
 }
